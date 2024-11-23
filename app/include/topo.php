839e015a5b8b3045ddd17d28bb38822e67becb36
Davi Aron <daviaronalfa@gmail.com>
2024-11-23 17:03:13 -0300

    criando arquivos para o perfil de medico

--- app/include/topo.php
+++ app/include/topo.php
@@ -5,5 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CodeX</title>
     <link rel="stylesheet" href="assets/css/globalStyle.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 </head>
 <body>  
